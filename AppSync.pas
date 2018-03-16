ae444282ee2a3b5bd96b0c4350e4a12162399573
LWAndy <andychr@163.com>
2018-03-16 14:58:44 +0800

    完成DELPHI同步ERP数据报表功能

--- AppSync.pas
+++ AppSync.pas
@@ -11,7 +11,8 @@ type
     { Private declarations }
     procedure CreateDBListTable;
     procedure CreateReportListTable;
-    function setReportDate(cdsTmp: TAppDataSet; const difTime,Status: Integer;const ErrorMsg: String = ''): Boolean;
+    function setReportDate(cdsTmp: TAppDataSet; const difTime, Status: Integer;const ErrorMsg: string = ''): Boolean;
+
   public
     cdsDBList: TZjhDataSet;
     cdsReportList: TZjhDataSet;
@@ -19,32 +20,14 @@ type
     procedure ReadReportList;
     procedure Init();
     procedure Destroy();
-    procedure Exec();
+    procedure Exec(); overload;
+    procedure Exec(DBUID: string); overload;
+    function getNewSQLCmd(oldSql, erpCode: string): string;
   end;
-
-  {
-  TThreadAppSync = class(TThread)
-  private
-    difTime: Integer;
-    cdsDBList: TZjhDataSet;
-    cdsReportList: TZjhDataSet;
-    procedure Init();
-    procedure ReadDBList;
-    procedure ReadReportList;
-    procedure CreateDBListTable;
-    procedure CreateReportListTable;
-    function setReportDate(cdsTmp: TAppDataSet; const difTime,Status: Integer;const ErrorMsg: String = ''): Boolean;
-  protected
-  public
-    constructor Create(CreateSuspended: Boolean); overload;
-    destructor Destroy; override;
-  public
-    procedure SetParams(difTime: Integer);
-    procedure Execute; override;
-  end;
-  }
 
 implementation
+
+uses MainFrm;
 
 { TAppSync }
 
@@ -88,6 +71,18 @@ begin
       DataType := ftWideString;
       Size := 5;
       Name := 'PortNo_';
+    end;
+    with FieldDefs.AddFieldDef do
+    begin
+      DataType := ftWideString;
+      Size := 8;
+      Name := 'ExecuteTime_';
+    end;
+    with FieldDefs.AddFieldDef do
+    begin
+      DataType := ftWideString;
+      Size := 20;
+      Name := 'ERPCode_';
     end;
     CreateDataSet;
   end;
@@ -136,7 +131,7 @@ begin
     with FieldDefs.AddFieldDef do
     begin
       DataType := ftWideString;
-      Size := 10;
+      Size := 50;
       Name := 'Ver_';
     end;
     CreateDataSet;
@@ -149,9 +144,9 @@ begin
   cdsReportList.Free;
 end;
 
-procedure TAppSync.Exec;
+procedure TAppSync.Exec(DBUID: string);
 var
-  sAccount,sPassword,sHost,DBName : String;
+  sAccount,sPassword,sHost,DBName : string;
   SQLCmd: String;
   cdsTmp: TADOQuery;
   cdsTmp1: TAppDataSet;
@@ -168,11 +163,115 @@ begin
   try
     cdsTmp.Connection := oCn;
     cdsDBList.First;
+    if cdsDBList.Locate('DBUID_', DBUID, []) then
+    begin
+      sAccount := cdsDBList.FieldByName('Account_').AsString;
+      sPassword := cdsDBList.FieldByName('Password_').AsString;
+      DBName := cdsDBList.FieldByName('Database_').AsString;
+      sHost :=  cdsDBList.FieldByName('Host_').AsString;
+      try
+        oCn.Close;
+        oCn.ConnectionString := Format('Provider=SQLOLEDB.1;Password=%s;Persist '
+          + 'Security Info=True;User ID=%s;Initial Catalog=%s;Data Source=%s',
+          [sPassword,sAccount,DBName,sHost]);
+        oCn.LoginPrompt := False;
+        oCn.Open;
+        cdsReportList.Filtered:= False;
+        cdsReportList.Filter:=Format('DBUID_=%s',[cdsDBList.FieldByName('DBUID_').AsString]);
+        cdsReportList.Filtered:=True;
+        cdsReportList.First;
+        while not cdsReportList.eof do
+        begin
+          SQLCmd := getNewSQLCmd(cdsReportList.FieldByName('Param_').AsString, cdsDBList.FieldByName('ERPCode_').AsString);
+          try
+            difTime := 0;
+            cdsTmp.Close;
+            cdsTmp.SQL.Text := SQLCmd;
+            d1 := now();
+            cdsTmp.Open;
+            difTime := Trunc(now()) - Trunc(d1);
+            if not cdsTmp.Eof then
+            begin
+              cdsTmp1 := TAppDataSet.Create;
+              try
+                cdsTmp1.LoadFrom(cdsTmp);
+                setReportDate(cdsTmp1, difTime, 1);
+              finally
+                cdsTmp1.Free;
+              end;
+            end;
+          except
+            on E: Exception do
+            begin
+              setReportDate(cdsTmp1, difTime, 0, E.Message);
+            end;
+          end;
+          cdsReportList.Next;
+        end;
+      except
+        on E: Exception do
+        begin
+          FrmMain.memBody.Lines.Add('error: ' + DBName + '-' +  E.Message);
+        end;
+      end;
+    end;
+  finally
+    oCn.Free;
+    cdsTmp.Free;
+  end;
+end;
+
+function TAppSync.getNewSQLCmd(oldSql, erpCode: string): string;
+var
+  i, j, z: Integer;
+  newSql: string;
+  function Replace(const Value, SubStr, RplStr: String): String;
+  var
+    i: Integer;
+    Data: String;
+  begin
+    Data := Value;
+    Result := '';
+    i := Pos(SubStr,data);
+    while i > 0 do
+    begin
+      Result := Result + Copy(Data,1,i-1) + RplStr;
+      Data := Copy(Data, i + Length(SubStr), Length(Data));
+      i := Pos(SubStr,Data);
+    end;
+    Result := Result + Data;
+  end;
+begin
+  newSql := oldSql;
+  if POS(UpperCase('@CorpCode'), UpperCase(oldSql))>0 then
+    newSql := Replace(oldSql,'@CorpCode', '''' + erpCode + '''');
+  Result := newSql;
+end;
+
+procedure TAppSync.Exec;
+var
+  sAccount, sPassword, sHost, DBName : string;
+  SQLCmd: String;
+  cdsTmp: TADOQuery;
+  cdsTmp1: TAppDataSet;
+  d1: TDateTime;
+  difTime: Integer;
+  oCn: TADOConnection;
+begin
+  if not cdsDBList.Active then Exit;
+  if cdsDBList.RecordCount = 0 then Exit;
+  if not cdsReportList.Active then Exit;
+  if cdsReportList.RecordCount = 0 then Exit;
+  oCn := TADOConnection.Create(nil);
+  cdsTmp := TADOQuery.Create(nil);
+  try
+    cdsTmp.Connection := oCn;
+    cdsDBList.First;
     while not cdsDBList.eof do
     begin
-      sAccount:= cdsDBList.FieldByName('Account_').AsString;
-      sPassword:= cdsDBList.FieldByName('Password_').AsString;
-      DBName:= cdsDBList.FieldByName('Database_').AsString;
+      sAccount := cdsDBList.FieldByName('Account_').AsString;
+      sPassword := cdsDBList.FieldByName('Password_').AsString;
+      DBName := cdsDBList.FieldByName('Database_').AsString;
       sHost :=  cdsDBList.FieldByName('Host_').AsString;
       try
         oCn.Close;
@@ -187,13 +286,13 @@ begin
           Continue;
         end;
       end;
-      cdsReportList.Filtered:= False;
-      cdsReportList.Filter:=Format('DBUID_=%s',[cdsDBList.FieldByName('DBUID_').AsString]);
-      cdsReportList.Filtered:=True;
+      cdsReportList.Filtered := False;
+      cdsReportList.Filter :=Format('DBUID_=%s',[cdsDBList.FieldByName('DBUID_').AsString]);
+      cdsReportList.Filtered :=True;
       cdsReportList.First;
       while not cdsReportList.eof do
       begin
-        SQLCmd := cdsReportList.FieldByName('Param_').AsString ;
+        SQLCmd := getNewSQLCmd(cdsReportList.FieldByName('Param_').AsString, cdsDBList.FieldByName('ERPCode_').AsString);
         try
           difTime := 0;
           cdsTmp.Close;
@@ -206,7 +305,7 @@ begin
             cdsTmp1 := TAppDataSet.Create;
             try
               cdsTmp1.LoadFrom(cdsTmp);
-              setReportDate(cdsTmp1, difTime,1);
+              setReportDate(cdsTmp1, difTime, 1);
             finally
               cdsTmp1.Free;
             end;
@@ -214,15 +313,13 @@ begin
         except
           on E: Exception do
           begin
-            //ShowMessage(E.Message);
-            setReportDate(cdsTmp1, difTime,0,E.Message);
+            setReportDate(cdsTmp1, difTime, 0, E.Message);
           end;
         end;
         cdsReportList.Next;
       end;
       cdsDBList.Next;
     end;
-    ShowMessage('执行完成');
   finally
     oCn.Free;
     cdsTmp.Free;
@@ -257,6 +354,8 @@ begin
           cdsDBList.FieldByName('Account_').AsString := FieldByName('Account_').AsString;
           cdsDBList.FieldByName('Password_').AsString := FieldByName('Password_').AsString;
           cdsDBList.FieldByName('PortNo_').AsString := FieldByName('PortNo_').AsString;
+          cdsDBList.FieldByName('ExecuteTime_').AsString := FieldByName('ExecuteTime_').AsString;
+          cdsDBList.FieldByName('ERPCode_').AsString := FieldByName('ERPCode_').AsString;
           cdsDBList.Post();
           Next;
         end;
@@ -296,8 +395,6 @@ begin
     ShowMessage(app.Messages);
 end;
 
-
-
 function TAppSync.setReportDate(cdsTmp: TAppDataSet; const difTime,
   Status: Integer; const ErrorMsg: String): Boolean;
 var
@@ -316,7 +413,12 @@ begin
   app.DataIn.FieldByName('Ver_').AsString := cdsReportList.FieldByName('Ver_').AsString;
   app.DataIn.FieldByName('Database_').AsString := cdsDBList.FieldByName('Database_').AsString;
   if app.Exec then
-    Result := True
+    begin
+      FrmMain.memBody.Lines.Add('Satus: ' + cdsDBList.FieldByName('Database_').AsString
+        + ' RetName:' + cdsReportList.FieldByName('RetName_').AsString + ';'
+        + ' T-Sql:' + cdsReportList.FieldByName('Param_').AsString + ' ' + ErrorMsg);
+      Result := True;
+    end
   else
     ShowMessage(app.Messages);
 end;
--- MainFrm.pas
+++ MainFrm.pas
@@ -5,53 +5,53 @@ interface
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
   Dialogs, ExtCtrls, StdCtrls, AppUtils2, Grids, DBGrids, DB, DBClient, ZjhCtrls,
-  Buttons, ComCtrls, ADODB, AppDB2, AppSync;
+  Buttons, ComCtrls, ADODB, AppDB2, AppSync,IniFiles, Menus,ShellAPI,Tlhelp32,
+  jpeg;
 
 type
   TFrmMain = class(TForm)
-    cdsView: TZjhDataSet;
-    dsView: TDataSource;
-    DBGrid1: TDBGrid;
     Panel2: TPanel;
-    cdsViewCorpNo_: TWideStringField;
-    cdsViewGroup_: TWideStringField;
-    cdsViewRetNo_: TWideStringField;
-    cdsViewRetName_: TWideStringField;
-    cdsViewParam_: TWideMemoField;
-    Panel3: TPanel;
-    Label4: TLabel;
-    cboRemoteHost: TComboBox;
-    Label5: TLabel;
-    edtRemoteUser: TEdit;
-    Label6: TLabel;
-    edtRemotePwd: TEdit;
-    BitBtn3: TBitBtn;
-    DBGrid2: TDBGrid;
+    PopupMenu1: TPopupMenu;
+    popShowForm: TMenuItem;
+    popHideForm: TMenuItem;
+    N2: TMenuItem;
+    C1: TMenuItem;
+    N1: TMenuItem;
+    N3: TMenuItem;
+    Timer1: TTimer;
     cdsDBList: TZjhDataSet;
-    dsDBList: TDataSource;
+    cdsDBListDBUID_: TWideStringField;
     cdsDBListDatabase_: TWideStringField;
     cdsDBListHost_: TWideStringField;
-    cdsDBListPassword_: TWideStringField;
     cdsDBListAccount_: TWideStringField;
+    cdsDBListPassword_: TWideStringField;
     cdsDBListPortNo_: TWideStringField;
-    cdsDBListDBUID_: TWideStringField;
-    cdsViewDBUID_: TWideStringField;
-    cdsSQL: TZjhDataSet;
-    cdsViewVer_: TWideStringField;
-    Label1: TLabel;
-    Button1: TButton;
+    cdsDBListExecuteTime_: TTimeField;
+    BtnOpen: TBitBtn;
+    cboRemoteHost: TComboBox;
+    edtRemotePwd: TEdit;
+    edtRemoteUser: TEdit;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    memBody: TMemo;
+    Image1: TImage;
     procedure FormCreate(Sender: TObject);
-    procedure BitBtn3Click(Sender: TObject);
-    procedure cdsViewParam_GetText(Sender: TField; var Text: string;
-      DisplayText: Boolean);
-    procedure Button1Click(Sender: TObject);
+    procedure BtnOpenClick(Sender: TObject);
+    procedure C1Click(Sender: TObject);
+    procedure popHideFormClick(Sender: TObject);
+    procedure popShowFormClick(Sender: TObject);
+    procedure N3Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
   private
     { Private declarations }
-    function setReportDate(cdsTmp: TAppDataSet; difTime: Integer): Boolean;
+    TrayIconData: TNotifyIconData;
+    Ini: TIniFile;
+    procedure OnTrayMessage(var Msg: TMessage); message WM_USER + 100;
+    procedure ReadDBList;
   public
     { Public declarations }
-    procedure ReadDBList;
-    procedure ReadReportList;
+    function KillTask(ExeFileName:string):boolean;
   end;
 
 var
@@ -61,108 +61,149 @@ implementation
 
 {$R *.dfm}
 
-procedure TFrmMain.BitBtn3Click(Sender: TObject);
+procedure TFrmMain.BtnOpenClick(Sender: TObject);
 var
   dReadOnly: Boolean;
 begin
+  if (cboRemoteHost.Text = '') then
+  begin
+    ShowMessage('请输入服务主机！');
+    Exit;
+  end;
+  if (edtRemoteUser.Text = '') then
+  begin
+    ShowMessage('请输入帐号！');
+    Exit;
+  end;
+  if (edtRemotePwd.Text = '') then
+  begin
+    ShowMessage('请输入密码！');
+    Exit;
+  end;
+  Ini := TIniFile.Create(ExtractFilePath(Application.ExeName)+'TAppSync.ini');
+  try
+    Ini.WriteString('TAppSync','Host', cboRemoteHost.Text);
+    Ini.WriteString('TAppSync','User', edtRemoteUser.Text);
+    //Ini.WriteString('TAppSync','Pwd', edtRemotePwd.Text);
+  finally
+    Ini.Free;
+  end;
   __ServerIP := cboRemoteHost.Text;
-  dReadOnly := loginToVine(edtRemoteUser.Text,edtRemotePwd.Text);
-  BitBtn3.Enabled := not dReadOnly;
+  dReadOnly := loginToVine(edtRemoteUser.Text, edtRemotePwd.Text);
+  if not dReadOnly then
+    begin
+      ShowMessage('登录失败');
+      Exit;
+    end
+  else
+    memBody.Lines.Add('登录成功');
+  ReadDBList;
+  timer1.Enabled := True;
+  BtnOpen.Enabled := not dReadOnly;
   cboRemoteHost.Enabled := not dReadOnly;
   edtRemoteUser.ReadOnly := dReadOnly;
   edtRemotePwd.ReadOnly := dReadOnly;
-  ReadDBList;
-  ReadReportList;
-  Button1.Enabled := dReadOnly;
-  //if dReadOnly then showmessage('成功連接');
 end;
 
-procedure TFrmMain.Button1Click(Sender: TObject);
-var
-  TSync: TAppSync;
+procedure TFrmMain.C1Click(Sender: TObject);
 begin
-  if __SessionID = '' then
-  begin
-    ShowMessage('请先登录');
-    Exit;
-  end;
-  TSync := TAppSync.Create;
-  try
-    TSync.Init;
-    TSync.ReadDBList;
-    TSync.ReadReportList;
-    TSync.Exec;
-    TSync.Destroy;
-  finally
-    TSync.Free;
-  end;
-end;
-
-procedure TFrmMain.cdsViewParam_GetText(Sender: TField; var Text: string;
-  DisplayText: Boolean);
-begin
-  Text := Sender.AsWideString;
+  Close;
 end;
 
 procedure TFrmMain.FormCreate(Sender: TObject);
 begin
-  cdsView.CreateDataSet;
   cdsDBList.CreateDataSet;
-  cboRemoteHost.Text :='127.0.0.1:8080';//'c1.knowall.cn';
-  edtRemoteUser.Text := '91100123';
-  edtRemotePwd.Text := '123456';
+  //cboRemoteHost.Items.Add('127.0.0.1:8080');
+  //cboRemoteHost.Items.Add('c1.knowall.cn');
+  Ini := TIniFile.Create(ExtractFilePath(Application.ExeName) + 'TAppSync.ini');
+  try
+    cboRemoteHost.Text := Ini.ReadString('TAppSync', 'Host', 'c1.knowall.cn');//
+    edtRemoteUser.Text := Ini.ReadString('TAppSync', 'User', '91100123');
+    //edtRemotePwd.Text := Ini.ReadString('TAppSync','Pwd','123456');
+  finally
+    Ini.Free;
+  end;
+  with TrayIconData do
+  begin
+    Wnd := Handle;
+    uID := 100;
+    uFlags := NIF_MESSAGE + NIF_ICON + NIF_TIP;
+    uCallbackMessage := WM_USER + 100;
+    hIcon := Application.Icon.Handle;
+    StrPCopy(szTip, Application.Title);
+  end;
+  Shell_NotifyIcon(NIM_ADD, @TrayIconData);
+  Application.OnMinimize := popHideFormClick;
 end;
 
-procedure TFrmMain.ReadReportList;
-var
-  app: IAppService;
-begin
-  app := Service('SvrERPSyncReport.getReportList');
-  if app.Exec then
-    begin
-      cdsView.EmptyDataSet;
-      with app.DataOut do
-      begin
-        First;
-        while not Eof do
-        begin
-          cdsView.Append;
-          cdsView.FieldByName('DBUID_').AsString := FieldByName('DBID_').AsString;
-          cdsView.FieldByName('CorpNo_').AsString := FieldByName('CorpNo_').AsString;
-          cdsView.FieldByName('Group_').AsString := FieldByName('Group_').AsString;
-          cdsView.FieldByName('RetNo_').AsString := FieldByName('RetNo_').AsString;
-          cdsView.FieldByName('RetName_').AsString := FieldByName('RetName_').AsString;
-          cdsView.FieldByName('Param_').AsString := FieldByName('Param_').AsString;
-          cdsView.FieldByName('Ver_').AsString := FieldByName('Ver_').AsString;
-          cdsView.Post();
-          Next;
-        end;
-      end;
-    end
-  else
-    ShowMessage(app.Messages);
-end;
 
-function TFrmMain.setReportDate(cdsTmp: TAppDataSet; difTime: Integer): Boolean;
+function TFrmMain.KillTask(ExeFileName: string): boolean;
+const
+  PROCESS_TERMINATE = $0001;
 var
-  app: IAppService;
+  ContinueLoop: BOOLean;
+  FSnapshotHandle: THandle;
+  FProcessEntry32: TProcessEntry32;
+  sFileName: String;
 begin
   Result := False;
-  app := Service('SvrERPSyncReport.setReportDate');
-  app.DataIn.Append();
-  app.DataIn.FieldByName('RetNo_').AsString := cdsView.FieldByName('RetNo_').AsString;
-  app.DataIn.FieldByName('TDate_').AsDateTime := Date();
-  app.DataIn.FieldByName('Status_').AsInteger := 1;
-  app.DataIn.FieldByName('Value_').AsString := cdsTmp.JSON.AsString;
-  app.DataIn.FieldByName('ErrorMsg_').AsString := '';
-  app.DataIn.FieldByName('Remark_').AsString := '';
-  app.DataIn.FieldByName('Times_').AsInteger := difTime;
-  app.DataIn.FieldByName('Ver_').AsString := cdsView.FieldByName('Ver_').AsString;
-  app.DataIn.FieldByName('Database_').AsString := cdsDBList.FieldByName('Database_').AsString;
-  if app.Exec then
-    Result := True
-  else
-    ShowMessage(app.Messages);
+  FSnapshotHandle := CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+  FProcessEntry32.dwSize := SizeOf(FProcessEntry32);
+  ContinueLoop := Process32First(FSnapshotHandle, FProcessEntry32);
+  while Integer(ContinueLoop) <> 0 do
+  begin
+    sFileName := UpperCase(ExtractFileName(FProcessEntry32.szExeFile));
+    if sFileName = UpperCase(ExeFileName)then
+      Result := TerminateProcess(OpenProcess(PROCESS_TERMINATE,
+                                    BOOL(0),
+                                    FProcessEntry32.th32ProcessID),
+                                    0);
+     ContinueLoop := Process32Next(FSnapshotHandle, FProcessEntry32);
+  end;
+  CloseHandle(FSnapshotHandle);
+end;
+
+procedure TFrmMain.N3Click(Sender: TObject);
+begin
+  memBody.Visible := not memBody.Visible;
+end;
+
+procedure TFrmMain.OnTrayMessage(var Msg: TMessage);
+var
+  P: TPoint;
+begin
+  case Msg.LParam of
+    WM_RBUTTONDOWN:
+      begin
+        GetCursorPos(P);
+        PopupMenu1.Popup(p.X, p.Y);
+      end;
+    WM_LBUTTONDBLCLK:
+    //WM_LBUTTONDOWN:
+      begin
+        if not popShowForm.Enabled then
+          popHideFormClick(Self)
+        else
+          popShowFormClick(Self);
+      end;
+  end;
+end;
+
+procedure TFrmMain.popHideFormClick(Sender: TObject);
+begin
+  popShowForm.Enabled := True;
+  popHideForm.Enabled := False;
+  Hide;
+  ShowWindow(Application.Handle,SW_HIDE);
+end;
+
+procedure TFrmMain.popShowFormClick(Sender: TObject);
+begin
+  popShowForm.Enabled := False;
+  popHideForm.Enabled := True;
+  Application.Restore;
+  Show;
+  Application.BringToFront;
 end;
 
 procedure TFrmMain.ReadDBList;
@@ -185,6 +226,7 @@ begin
           cdsDBList.FieldByName('Account_').AsString := FieldByName('Account_').AsString;
           cdsDBList.FieldByName('Password_').AsString := FieldByName('Password_').AsString;
           cdsDBList.FieldByName('PortNo_').AsString := FieldByName('PortNo_').AsString;
+          cdsDBList.FieldByName('ExecuteTime_').AsDateTime := FieldByName('ExecuteTime_').AsDateTime;
           cdsDBList.Post();
           Next;
         end;
@@ -192,6 +234,53 @@ begin
     end
   else
     ShowMessage(app.Messages);
+end;
+
+procedure TFrmMain.Timer1Timer(Sender: TObject);
+var
+  TSync: TAppSync;
+  DBList: TStringList;
+  i: Integer;
+  DBUID: string;
+begin
+  Timer1.Enabled := False;
+  DBList := TStringList.Create;
+  try
+    cdsDBList.First;
+    while not cdsDBList.Eof do
+    begin
+      if FormatDateTime('hh:mm', Now) = FormatDateTime('hh:mm',cdsDBList.FieldByName('ExecuteTime_').AsDateTime) then
+        DBList.Add(cdsDBList.FieldByName('DBUID_').AsString);
+      cdsDBList.Next;
+    end;
+    if DBList.Count > 0 then
+    begin
+      for i := 0 to DBList.Count - 1 do
+      begin
+        DBUID := DBList[i];
+        TSync := TAppSync.Create;
+        try
+          try
+            TSync.Init;
+            TSync.ReadDBList;
+            TSync.ReadReportList;
+            TSync.Exec(DBUID);
+            TSync.Destroy;
+          except
+            on E: Exception do
+            begin
+              FrmMain.memBody.Lines.Add('error: ' + E.Message);
+            end;
+          end;
+        finally
+          TSync.Free;
+        end;
+      end;
+    end;
+  finally
+    Timer1.Enabled := True;
+    DBList.Free;
+  end;
 end;
 
 end.
